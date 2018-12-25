d35238d94a14f81ac5392eafd9265d9af5871b96
KC2A3 <947496351@qq.com>
2018-12-26 03:40:53 +0800

    Add id to the character，add introduceWith method

--- main/practice_8/student.js
+++ main/practice_8/student.js
@@ -1,1 +1,15 @@
-
+import Person from "./person.js"
+class Student extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    if (this.klass.leader === this) {
+      return `${super.introduce()} I am a Student. I am leader of ${this.class.number}.`;
+    } else {
+      return `${super.introduce()} I am a Student. I am at ${this.class.number}.`;
+    }
+  }
+}
+export default Student;
