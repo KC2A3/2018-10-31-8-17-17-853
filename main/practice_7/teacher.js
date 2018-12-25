a37b3c715fadabbb52c69058d59a8f6276715da9
KC2A3 <947496351@qq.com>
2018-12-26 03:38:38 +0800

    Create class attribute

--- main/practice_7/teacher.js
+++ main/practice_7/teacher.js
@@ -1,1 +1,22 @@
-
+import Person from './person.js';
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    if (this.klass) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}`;
+    } else {
+      return `${super.introduce()} I am a Teacher. I teach No Class`;
+    }
+  }
+  introduceWith(student) {
+    if (student.klass.number === this.klass.number) {
+      return `${super.introduce()}I am a Teacher. I teach ${student.name}.`;
+    } else {
+      return `${super.introduce()}I am a Teacher. I don't teach ${student.name}.`;
+    }
+  }
+}
+export default Teacher;
