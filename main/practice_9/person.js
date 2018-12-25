bb5b4e9d578556494b2e72750fce0107f93f748d
KC2A3 <947496351@qq.com>
2018-12-26 03:42:44 +0800

    Add appendMember method to the class

--- main/practice_9/person.js
+++ main/practice_9/person.js
@@ -1,1 +1,11 @@
-
+class Person {
+  constructor(name, age, id) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+}
+export default Person;
