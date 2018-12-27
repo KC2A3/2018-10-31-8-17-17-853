ab9c742d4bda8895d7de8d9a72adcd381232a613
KC2A3 <947496351@qq.com>
2018-12-28 02:26:37 +0800

    Add two printing methods for the teacher

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,31 @@
-
+class Class {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+  registerAssignLeaderListener(teacher) {
+    this.assignLeaderListener = teacher;
+  }
+  registerJoinListener(teacher) {
+    this.joinListener = teacher;
+  }
+  assignLeader(student) {
+    if (student.klass === this) {
+      this.leader = student;
+      if (this.assignLeaderListener) {
+        this.assignLeaderListener.declareAssignLeader(student, this);
+      }
+    } else {
+      console.log 'It is not one of us.';
+    }
+  }
+  appendMember(student) {
+    student.klass = this;
+  }
+  isIn(student) {
+    return this === student.klass;
+  }
+}
+export default Class;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,24 @@
-
+import Person from "./person.js";
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+  introduce() {
+    if (this.klasses) {
+      let classNumber = this.klasses.map(klass => klass.number).join(', ')
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klass.number}`;
+    } else {
+      return `${super.introduce()} I am a Teacher. I teach No Class`;
+    }
+  }
+  isTeaching(student) {
+    for (let klass of klasses) {
+      if (klass.isIn(student)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+export default Teacher;
